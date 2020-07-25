f296745104bca867450fab7e5fa73d5c1062c004
Mohamed <mohamed.ammar86@gmail.com>
2020-07-25 15:20:37 -0400

    app is running

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-const https = require("https");
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
 
@@ -10,39 +12,40 @@ res.sendFile(__dirname + "/index.html");
 
 });
 
-app.post("/", function(){
+app.post("/", function(req, res){
 
-  console.log("post successul");
+  const city = req.body.cityName;
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=46911f906cadc2dcfe25045b2f3726ef&units=metric";
+
+  https.get(url, function(response){
+
+    console.log(response);
+
+      response.on("data", function(data){
+      const weatherData = JSON.parse(data);
+
+      const weatherDes = weatherData.weather[0].description;
+      const temp = weatherData.main.temp;
+      const icon = weatherData.weather[0].icon;
+      const iconLink = "http://openweathermap.org/img/wn/"+ icon + "@2x.png";
+
+      res.write("<p>The weather in " + city + " is currently " + weatherDes + "</p>");
+      res.write("<h1>The temperature in " + city + " is currently " + temp + " celcius</h1>");
+      res.write("<img src="+ iconLink +" >")
+
+      res.send();
+
+
+
+    })
+  })
+
+
+
 
 })
 
-
-
-// const url = "https://api.openweathermap.org/data/2.5/weather?q=sacramento&appid=46911f906cadc2dcfe25045b2f3726ef&units=metric";
-//
-// https.get(url, function(response){
-//
-//   console.log(response);
-//
-//   response.on("data", function(data){
-//     const weatherData = JSON.parse(data);
-//     console.log(weatherData);
-//
-//     const weatherDes = weatherData.weather[0].description;
-//     const temp = weatherData.main.temp;
-//     const icon = weatherData.weather[0].icon;
-//     const iconLink = "http://openweathermap.org/img/wn/"+ icon + "@2x.png";
-//
-//     res.write("<p>The weather in Tampa is currently " + weatherDes + "</p>");
-//     res.write("<h1>The temperature in Tampa is currently " + temp + " celcius</h1>");
-//     res.write("<img src="+ iconLink +" >")
-//
-//     res.send();
-//
-//
-//
-//   })
-// })
 
 
 
