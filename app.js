631e85645525eb0b52d13ba3b20408e1b542a731
Mohamed <mohamed.ammar86@gmail.com>
2020-07-25 12:45:51 -0400

    second update

--- app.js
+++ app.js
@@ -6,38 +6,43 @@ const https = require("https");
 
 app.get("/", function(req, res){
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=sacramento&appid=46911f906cadc2dcfe25045b2f3726ef&units=metric";
-
-  https.get(url, function(response){
-
-    console.log(response);
-
-    response.on("data", function(data){
-      const weatherData = JSON.parse(data);
-      console.log(weatherData);
-
-      const weatherDes = weatherData.weather[0].description;
-      const temp = weatherData.main.temp;
-      const icon = weatherData.weather[0].icon;
-      const iconLink = "http://openweathermap.org/img/wn/"+ icon + "@2x.png";
-
-      res.write("<p>The weather in Tampa is currently " + weatherDes + "</p>");
-      res.write("<h1>The temperature in Tampa is currently " + temp + " celcius</h1>");
-      res.write("<img src="+ iconLink +" >")
-
-      res.send();
-
-
-
-    })
-  })
-
+res.sendFile(__dirname + "/index.html");
 
 });
 
+app.post("/", function(){
+
+  console.log("post successul");
+
+})
 
 
 
+// const url = "https://api.openweathermap.org/data/2.5/weather?q=sacramento&appid=46911f906cadc2dcfe25045b2f3726ef&units=metric";
+//
+// https.get(url, function(response){
+//
+//   console.log(response);
+//
+//   response.on("data", function(data){
+//     const weatherData = JSON.parse(data);
+//     console.log(weatherData);
+//
+//     const weatherDes = weatherData.weather[0].description;
+//     const temp = weatherData.main.temp;
+//     const icon = weatherData.weather[0].icon;
+//     const iconLink = "http://openweathermap.org/img/wn/"+ icon + "@2x.png";
+//
+//     res.write("<p>The weather in Tampa is currently " + weatherDes + "</p>");
+//     res.write("<h1>The temperature in Tampa is currently " + temp + " celcius</h1>");
+//     res.write("<img src="+ iconLink +" >")
+//
+//     res.send();
+//
+//
+//
+//   })
+// })
 
 
 
